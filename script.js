aff67fcfbfd1610751fca2fdb142b48a8624cc21
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-05-05 03:11:41 -0400

    used firstChild

--- script.js
+++ script.js
@@ -8,3 +8,5 @@ percentage.style.textAlign = "center";
 
 document.querySelector("h3").innerHTML = "<strong>Age</strong> and <strong>Gender</strong> play a huge roll because...um well...I have no clue just know it's <i>IMPORTANT!</i>"
 
+let best = document.getElementById("myList").firstChild.innerHTML;
+document.getElementById("theBest").innerHTML = best;
