5a6d28d7905bd7e74e6ec8bbff9fe4b98e8974d2
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-05-05 02:47:19 -0400

    querySelector, innerHTML, some css

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const openerText = document.getElementById("opener");
 
-openerText.style.color = "white"
+openerText.style.color = "white";
 
 const percentage = document.getElementById("percentage");
 
-percentage.style.textAlign = "center"
+percentage.style.textAlign = "center";
 
-
+document.querySelector("h3").innerHTML = "<strong>Age</strong> and <strong>Gender</strong> play a huge roll because...um well...I have no clue just know it's <i>IMPORTANT!</i>"
 
